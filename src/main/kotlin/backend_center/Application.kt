2b22a75f22bc125ebea3087f2d5346a7d0057bc0
williemwang <1092473788@qq.com>
2023-05-18 16:12:50 +0800

    chore:init

---
+++ src/main/kotlin/backend_center/Application.kt
@@ -0,0 +1,16 @@
+package backend_center
+
+import backend_center.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.tomcat.*
+
+fun main() {
+    embeddedServer(Tomcat, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/test/kotlin/backend_center/ApplicationTest.kt
@@ -0,0 +1,26 @@
+package backend_center
+
+import backend_center.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+
+    @Test
+    fun testLoop() {
+        val items = listOf("")
+    }
+}
