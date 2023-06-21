d7f7945cc3661001de52e02a2878c33e9ee4772c
williemwang <1092473788@qq.com>
2023-06-21 18:06:22 +0800

    feat 完成平时成绩的录入

--- src/main/kotlin/backend_center/utils/ExcelUtils.kt
+++ src/main/kotlin/backend_center/utils/ExcelUtils.kt
@@ -1,12 +1,13 @@
 package backend_center.utils
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.ss.usermodel.Sheet
-import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.ss.formula.WorkbookEvaluator
+import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.InputStream
 
 /**
@@ -24,34 +25,80 @@ object ExcelUtils {
     )
 
     /**
-     * 将表中的指定列名根据基础表的列名规则，进行重新插入
-     */
-    private fun insertCellColumnToBaseExcelPos(excelSheet: Sheet) {
-
-    }
-
-    /**
-     * 根据基础表的姓名名单，将每一行移动至匹配位置
-     * 思路
-     * 1.
-     */
-    private fun moveRowOfCellByBaseExcel(excelSheet: Sheet): Sheet {
-        val row = excelSheet.physicalNumberOfRows
-
-        return excelSheet
-    }
-
-    /**
      * 将基础表数据填入指定表格中
      * @param baseSheet 基础表数据
-     * @param waitForInputSheet 等待填入数据的表格
+     * @param dataSheet 等待填入数据的表格
      * 思路
      * 1. 获取指定sheet页面中的数据
      * 2. 将获取到的数据填入到右表中指定位置，注意平时成绩需要先转换成百分制，在插入
      * 3. 最后成绩登记表中的总分使用公式
      */
-    private fun handleExcel(baseSheet: Sheet, waitForInputSheet: Sheet) {
+    private fun handleExcel(baseWorkbook: Workbook, baseSheet: Sheet, dataWorkbook: Workbook, dataSheet: Sheet, outputPath: String) {
+        val baseRowLength = baseSheet.physicalNumberOfRows
+        val dataRowLength = dataSheet.physicalNumberOfRows
+        val formulaEvaluator: FormulaEvaluator =
+            baseWorkbook.creationHelper.createFormulaEvaluator()
+        var baseExcelRow: Row
+        var baseExcelCell: Cell
+        var dataRow: Row
+        var dataCell: Cell
+        // 因为每个基础excel不太一样，需要先找到姓名那一列
+        val nameCellIndex = baseSheet.getRow(0).lastCellNum
+        var locateIndex = 0
+        for (nameIndex in 1..nameCellIndex) {
+            val name = baseSheet.getRow(0).getCell(nameIndex).stringCellValue
+            if (name == "姓名") {
+                locateIndex = nameIndex
+                break
+            }
+        }
+        // 遍历基础的excel的数据行
+        for (index in 1..baseRowLength) {
+            baseExcelRow = baseSheet.getRow(index - 1)
+            baseExcelCell = baseExcelRow.getCell(locateIndex)
+            // 遍历业务表的excel的数据行
+            for (dataIndex in 1..dataRowLength) {
+                // 根据上面定位的名字列位置，将值与业务表中的姓名一致的情况下，将值进行插入
+                dataRow = dataSheet.getRow(dataIndex - 1)
+                dataCell = dataRow.getCell(1)
+                // 直接匹配名字一致的情况下，将数据直接插入，但是要跳过姓名那一列
+                if (baseExcelCell.stringCellValue.replace("\\s".toRegex(), "") == dataCell.stringCellValue.replace("\\s".toRegex(), "")
+                ) {
+                    // 平时成绩
+                    try {
+                        val cellValue = baseExcelRow.getCell(baseExcelRow.lastCellNum.toInt() - 2)
+                        // 字符串时，跳出本循环
+                        if (cellValue.cellType == CellType.STRING && cellValue.stringCellValue.indexOf("成绩") != -1) {
+                            continue
+                        }
+                        var evaluatedCellValue: Double
+                        // 需要注意当表格类型为公式时，将其计算并返回
+                        if (cellValue.cellType == CellType.FORMULA) {
+                            evaluatedCellValue =
+                                formulaEvaluator.evaluate(cellValue).numberValue
+                        } else {
+                            // 数字为数值类型
+                            evaluatedCellValue =
+                                cellValue.numericCellValue
+                        }
 
+                        println(baseExcelRow.getCell(1))
+
+                        val fortyConvert100 = (evaluatedCellValue * 100) / 40
+                        println(fortyConvert100)
+                        dataRow.getCell(2).setCellValue(fortyConvert100.toString())
+                    } catch (e: NumberFormatException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+        // 最后将数据保存下来即可
+        val fileOutputStream = FileOutputStream(outputPath)
+        dataSheet.workbook.write(fileOutputStream)
+        fileOutputStream.close()
+        dataSheet.workbook.close()
     }
 
     /**
@@ -97,11 +144,12 @@ object ExcelUtils {
             /**
              * 不能为源文件并且不能为隐藏文件
              */
-            if (file.name.indexOf("期末考试统计表(1)") == -1  && !file.isHidden) {
-                waitForInputWorkbook = commonHandleExcel(FileInputStream("$baseFile\\${file.name}"), File("$baseFile\\${file.name}"))
+            if (file.name.indexOf("期末考试统计表(1)") == -1 && !file.isHidden) {
+                waitForInputWorkbook =
+                    commonHandleExcel(FileInputStream("$baseFile\\${file.name}"), File("$baseFile\\${file.name}"))
                 sheet = workbook.getSheet(file.nameWithoutExtension)
-                waitForInputSheet = waitForInputWorkbook.getSheet(file.nameWithoutExtension)
-                handleExcel(sheet, waitForInputSheet)
+                waitForInputSheet = waitForInputWorkbook.getSheetAt(0)
+                handleExcel(workbook, sheet, waitForInputWorkbook, waitForInputSheet, "$baseFile\\${file.name}")
             }
         }
 
